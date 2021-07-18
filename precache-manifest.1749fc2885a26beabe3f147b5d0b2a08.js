69b258a9c7c91581c2d2712656a531305d647f10
Rahul7549 <67723224+Rahul7549@users.noreply.github.com>
2021-07-18 13:54:39 +0530

    Updates

---
+++ precache-manifest.1749fc2885a26beabe3f147b5d0b2a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e048cc09475e925bdf1737947db959",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "9a709b310fdc68b54d81",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "9ba08b460bc57c194b76",
+    "url": "/covid-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "9a709b310fdc68b54d81",
+    "url": "/covid-tracker/static/js/2.e2750657.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-tracker/static/js/2.e2750657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba08b460bc57c194b76",
+    "url": "/covid-tracker/static/js/main.31aec767.chunk.js"
+  },
+  {
+    "revision": "1af475054be274cc1e5c",
+    "url": "/covid-tracker/static/js/runtime-main.9cfae146.js"
+  }
+]);
